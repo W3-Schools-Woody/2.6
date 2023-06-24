fe6a0f8f30cd811280ceb8c5d046a073e61f1e9b
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-06-24 14:33:59 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// program to solve quadratic equation
+let root1, root2;
+
+// take input from the user
+let a = promot("Enter first number:");
+let b =  prompt("Enter second number:");
+let c = prompt("Enter third number:");
+
+
+// calculate discriminant
+let discriminant = b * b - 4 * a * c;
+
+// condition for real and different roots
+if (discriminant > 0) {
+    root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+    // result
+    console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+};
+
+
+cited from:
+https://www.programiz.com/javascript/examples/quadratic-roots
